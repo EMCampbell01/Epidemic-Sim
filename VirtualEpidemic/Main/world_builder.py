fd3440fd49fda0c65f465cfbce53458d47428b0b
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-05-24 05:54:34 +0100

    Update world_builder.py

--- VirtualEpidemic/Main/world_builder.py
+++ VirtualEpidemic/Main/world_builder.py
@@ -2,10 +2,10 @@ import random
 
 
 # Each instance of Bob represents a individual in the epidemic
-class Bob():
+class Pop():
 
-    def __init__(self, bob_id, home_id, work_id, infected):
-        self.id = bob_id             # holds the unique ID for each bob
+    def __init__(self, pop_id, home_id, work_id, infected):
+        self.id = pop_id             # holds the unique ID for each bob
         self.home_id = home_id       # holds the home ID that the bob is assigned to
         self.work_id = work_id       # holds the work ID that the bob is assigned to
         self.infected = infected     # holds boolean for if the bob is infected or not
@@ -15,7 +15,6 @@ class Bob():
     day_recovered = 0
 
     # health status
-    recovered = False
     immune = False
     dead = False
 
@@ -27,42 +26,52 @@ class Pathogen():
         self.name = name                                  # Name of the pathogen
         self.infectivity_indoors = infectivity_indoors    # % chance of infection on exposure at home
         self.infectivity_outdoors = infectivity_outdoors  # % chance of infection on exposure at work
-        self.v_duration = v_duration                      # duration of time which an individual remains infected/infectious
-        self.i_duration = i_duration                      # duration of time which an individual remains immune
+        self.v_duration = v_duration                      # duration of time which an individual is infected/infectious
+        self.i_duration = i_duration                      # duration of time which an individual is immune
         self.mortality = mortality                        # % chance of dying from infection
 
 
+# Each instance of Household represents a home populated by pops
 class Household():
 
     def __init__(self, id):
         self.id = id
-        self.bobs_inside = None
+        self.pops_inside = None
         self.bobs_infected = None
 
 
+# Each instance of Workplace represents a workplace pops are designated to
 class Workplace():
 
-    def __init__(self, id, bobs_inside):
+    def __init__(self, id, pops_inside):
         self.id = id
-        self.bobs_inside = bobs_inside
+        self.pops_inside = pops_inside
 
 
-def summon_bobs(num, h_size, w_size):
-    bobs_population = num
+'''
+BUILD POPS
+
+build_pops generates and returns a list of pops
+pops are assigned to homes in chronological order and workplaces randomly
+'''
+
+
+def build_pops(population, h_size, w_size):
+    pops_population = population
     home_size = h_size
     work_size = w_size
-    bobs_list = []
+    pops_list = []
 
     building_world = True
-    bobs_created = 0
+    pops_created = 0
     current_home = 0
     current_home_pop = 0
     current_work = 0
     current_work_pop = 0
     while building_world:
-        if bobs_created != bobs_population:
-            bobs_list.append(Bob(bobs_created, current_home, 0, False))
-            bobs_created += 1
+        if pops_created != pops_population:
+            pops_list.append(Pop(pops_created, current_home, 0, False))
+            pops_created += 1
 
             if current_home_pop == home_size - 1:
                 current_home = current_home + 1
@@ -71,8 +80,8 @@ def summon_bobs(num, h_size, w_size):
                 current_home_pop = current_home_pop + 1
 
         else:
-            random.shuffle(bobs_list)
-            for bob in bobs_list:
+            random.shuffle(pops_list)
+            for bob in pops_list:
                 bob.work_id = current_work
                 if current_work_pop == work_size - 1:
                     current_work = current_work + 1
@@ -80,11 +89,12 @@ def summon_bobs(num, h_size, w_size):
                 else:
                     current_work_pop = current_work_pop + 1
 
-            bobs_list.sort(key=lambda x: x.id)
+            pops_list.sort(key=lambda x: x.id)
             building_world = False
-    return bobs_list
+    return pops_list
 
 
+# returns a list containing a specified number of homes
 def build_homes(num):
     home_number = num
     homes_list = []
@@ -96,6 +106,7 @@ def build_homes(num):
     return homes_list
 
 
+# returns a list containing a specified number of workplaces
 def build_workplaces(num):
     workplace_number = num
     workplace_list = []
