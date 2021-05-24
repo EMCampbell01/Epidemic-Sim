5dbd9c08a4bfc4b3751e651cedd97a47a6bd8c27
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-05-24 05:53:43 +0100

    Update main.py

--- VirtualEpidemic/Main/main.py
+++ VirtualEpidemic/Main/main.py
@@ -3,54 +3,66 @@ from Main import world_simulation as ws
 from Main import Visualization as Vz
 
 # Default world attributes
-bob_population = 1000
+population = 1000
 home_count = 250
 work_count = 125
 home_size = 4
 work_size = 8
 
 # Default pathogen attributes
-indoor_infectivity = 10
-outdoor_infectivity = 1
-p_duaration = 20
+indoor_infectivity = 2
+outdoor_infectivity = 2
+p_duration = 14
 i_duration = 32
-mortality = 2
+mortality = 10
 
-Vz.startup()
+# Generates instance of default pathogen
+pathogen = wb.Pathogen("Default Pathogen", indoor_infectivity, outdoor_infectivity, p_duration, i_duration, mortality)
 
+Vz.startup()  # Print title
+
+# Option to use default / configure settings
 answer = input("Use default settings? ")
 use_default = None
 
-if "Y" in answer:
+# Use default settings
+if "Y" in answer or "y" in answer:
     use_default = True
     print("Using Default Settings!")
 
-if "N" in answer:
+# Configure custom settings
+if "N" in answer or "n" in answer:
     use_default = False
     print("Configure Custom Settings!\n")
 
-if use_default == False:
+if not use_default:
 
+    # Set world attributes
     print("Configure Environment:")
-    bob_population = int(input("Population = "))
+    population = int(input("Population = "))
     home_count = int(input("Home Count = "))
     work_count = int(input("Work Count = "))
     home_size = int(input("Home Size = "))
     work_size = int(input("Work Size = "))
 
-    print("Configure Pathogen:")
+    # Set pathogen attributes
+    print("\nConfigure Pathogen:")
+    pathogen.name = input("Pathogen Name = ")
     indoor_infectivity = int(input("Indoor Infectivity % = "))
     outdoor_infectivity = int(input("Outdoor Infectivity % = "))
-    p_duaration = int(input("Infection Duration = "))
+    p_duration = int(input("Infection Duration = "))
     i_duration = int(input("Immunity Duration = "))
     mortality = int(input("Mortality % = "))
 
-bob_list = wb.summon_bobs(bob_population, home_size, work_size)
+# Generate world, population and virus according to selected attributes
 home_list = wb.build_homes(home_count)
 workplace_list = wb.build_workplaces(work_count)
+pop_list = wb.build_pops(population, home_size, work_size)
 
-virus = wb.Pathogen("Default Pathogen", indoor_infectivity, outdoor_infectivity, p_duaration, i_duration, mortality)
+# Runs the virtual epidemic and returns a list containing generated data
+daily_data_lists = (ws.run_simulation(pop_list, home_list, workplace_list, pathogen))
 
-daily_data_lists = (ws.run_simulation(bob_list, home_list, workplace_list, virus))
+# Plots and visualizes data
+Vz.visualization(daily_data_lists, pathogen.name)
+Vz.line_visualization(daily_data_lists)
 
-Vz.visualization(daily_data_lists, virus.name)
